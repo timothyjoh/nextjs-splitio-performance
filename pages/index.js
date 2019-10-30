b76e004b8d489013ba5f964d5d49c5a9a0ed40d6
Tim Johnson <jhnsntmthy@gmail.com>
2019-10-30 11:47:50 -0400

    stubbed out the simplest

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <style jsx>
+    {`
+      h1 {
+        font-family: 'Helvetica Neue', Helvetica, sans-serif;
+        font-size: 25em;
+        margin: 0px; padding: 0px;
+      }
+    `}
+    </style>
+    <h1>Control</h1>
+  </div>
+);
+
+export default Index;
